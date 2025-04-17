17c6274a206ca3c97585b1c96ad2de32a5d9994b
Thais Ventura <156233037+thaispng@users.noreply.github.com>
2025-04-17 18:54:10 -0300

    Arquitetura de projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './assets/images/logo.svg';
+import './assets/styles/global.css';
 function App() {
   return (
     <div className="App">
