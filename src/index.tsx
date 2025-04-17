afa5c51da19f5a22fa113f4c109309ba31687f86
Thais Ventura <156233037+thaispng@users.noreply.github.com>
2025-04-17 19:21:11 -0300

    refactor: simplify index.tsx by removing unnecessary imports and rendering logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './assets/styles/reset.css'; 
-import App from './App';
-import reportWebVitals from './lib/reportWebVitals'; 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// Se quiser medir a performance:
-reportWebVitals();
+import './main';
