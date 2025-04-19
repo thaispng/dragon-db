edb8b9ee85c17def7b26d1fd83047fa3bddaaebf
Thais Ventura <156233037+thaispng@users.noreply.github.com>
2025-04-19 02:40:12 -0300

    feat: adicionar ThemeToggle ao componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './assets/styles/global.css';
 import { DragonsListPage } from './features/dragons/pages/DragonsListPage'; // Importando o DragonsListPage
+import { ThemeToggle } from './components/ThemeToggle';
 
 function App() {
   return (
     <div className="App">
+      <>
+      <ThemeToggle />
       <DragonsListPage />
+      
+      </>
     </div>
   );
 }
