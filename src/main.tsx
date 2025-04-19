b6ee7aa89b495863219a7de48fc2c6307ca8ac46
Thais Ventura <156233037+thaispng@users.noreply.github.com>
2025-04-19 02:40:01 -0300

    feat: encapsular componente de aplicativo com ThemeProvider para gerenciamento de temas

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './assets/styles/reset.css';
-import App from './App';
-import reportWebVitals from './lib/reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import "./assets/styles/reset.css"
+import App from "./App"
+import reportWebVitals from "./lib/reportWebVitals"
+import { ThemeProvider } from "./context/ThemeContext"
+import "./assets/styles/theme.css"
 
-const queryClient = new QueryClient();
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const queryClient = new QueryClient()
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
 
-reportWebVitals();
+reportWebVitals()
